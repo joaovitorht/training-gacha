e3ef205a24a2d0faa23898874adc6f4d6f5294bb
joaovitorht <jvaguiar585@gmail.com>
2026-08-12 12:03:04 -0400

    Configura backend e conexão com PostgreSQL

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pool = require('./db/connection');
 
 const app = express();
 
@@ -10,7 +11,24 @@ app.get('/', (req, res) => {
   });
 });
 
-const PORT = 3000;
+app.get('/db-test', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+
+    res.json({
+      message: 'Banco conectado com sucesso!',
+      time: result.rows[0].now
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: 'Erro ao conectar com o banco'
+    });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
